9182f6553c775e94029dc28155c43698468aba45
akintolatech <akintolatechnologies@gmail.com>
2024-08-08 00:08:58 +0100

    Strings updt

--- Strings Documentation/string_doc.py
+++ Strings Documentation/string_doc.py
@@ -1,20 +1,23 @@
 """
 Programming Python
 Desc: Strings Operations in Python
-All rights reserved Lotus Studios Feb 2021
+All rights reserved Akintola Technology Feb 2021 updt AUG 24
 """
 
 text = "ivy is a girl"
 
 # string interpolation
-# print(f"the string is {text} and the length is {len(text)} and its type is {type(text)}")
+print("String Interpolation")
+print(f"the string is {text} and the length is {len(text)} and its type is {type(text)}")
 
 # string concatenation
-# print("the string is " + text + " and the length is " + str(len(text)) + " and its data type is " + str(type(text)))
+print("String Concatenation")
+print("the string is " + text + " and the length is " + str(len(text)) + " and its data type is " + str(type(text)))
 
 # string indexing
 text = "victor"
 
+# Slicing operations
 
 slice = text[3:]
 print(slice)
