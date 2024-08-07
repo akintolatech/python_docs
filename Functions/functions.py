c36b7fe269c051887bd3fd88812c31bc77f5a995
akintolatech <akintolatechnologies@gmail.com>
2024-08-07 23:51:08 +0100

    Functions updt

--- Functions/functions.py
+++ Functions/functions.py
@@ -6,6 +6,7 @@ def add(num1, num2):
     sum = num1 + num2
     return f"The sum is {sum}"
 
+
 # print(add(23,45))
 """
 Lotus Python Documentation
@@ -13,22 +14,27 @@ Desc: Function statements
 All right reserved lotus code studios Apr 2021
 """
 
+
 # A function that accepts multiple number of arguements
 def avg(first, *rest):
-    avg = first + sum(*rest)/ (1 + len(*rest))
+    avg = first + sum(*rest) / (1 + len(*rest))
     return avg
+
 
 # print(avg(1, [2, 3, 4]))
 
 # To accept any number of keyword arguments, use an argument that starts with **. For
 # example:
 import html
+
+
 def make_element(name, value, **attrs):
-    keyvals = ['%s="%s"'% item for item in attrs.items()]
+    keyvals = ['%s="%s"' % item for item in attrs.items()]
     attr_str = "".join(keyvals)
     value = html.escape(value)
     element = f"<{name} {attrs}>{value}</{name}>"
     return element
+
 
 # print(make_element("p", "value"))
 
@@ -40,7 +46,8 @@ def sum_list(arr):
         sum += i
     return sum
 
-arr = [1, 2, 4, 4, 5, 6, 7, 8, 9, 10 ]
+
+arr = [1, 2, 4, 4, 5, 6, 7, 8, 9, 10]
 
 total = sum_list(arr)
 
