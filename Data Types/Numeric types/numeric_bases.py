f86e084fe4e3f072757c16a02c4cdd669387acfe
akintolatech <akintolatechnologies@gmail.com>
2024-08-11 23:24:11 +0100

    Strings updt

---
+++ Data Types/Numeric types/numeric_bases.py
@@ -0,0 +1,13 @@
+"""
+Programming Python
+Desc: Numeric bases
+All right reserved Akintola Technologies Aug 24
+"""
+
+# hexadecimal numbers
+hex_num = 100
+print(f"Hexadecimal representation: {hex(hex_num)}")
+
+# octal numbers
+oct_num = 100
+print(f"Octal representation representation: {oct(oct_num)}")
