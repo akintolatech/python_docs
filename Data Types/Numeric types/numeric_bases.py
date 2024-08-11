1c782d2f6159e6deae85e764b47c25e366e748b4
akintolatech <akintolatechnologies@gmail.com>
2024-08-11 23:26:37 +0100

    Strings updt

--- Data Types/Numeric types/numeric_bases.py
+++ Data Types/Numeric types/numeric_bases.py
@@ -6,13 +6,13 @@ All right reserved Akintola Technologies Aug 24
 
 # binary numbers base 2
 bin_num = 100
-print(f"Binary number representation: {oct(bin_num)}")
+print(f"Binary number representation: {bin(bin_num)}")
 
-# octal numbers
+# octal numbers base 8
 oct_num = 100
 print(f"Octal number representation: {oct(oct_num)}")
 
-# hexadecimal numbers
+# hexadecimal numbers base 16
 hex_num = 100
 print(f"Hexadecimal number representation: {hex(hex_num)}")
 
